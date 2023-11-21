67e442ef2a0edff68b48a007bb41da42bc9d5cad
Alex Currie-Clark <acurrieclark@gmail.com>
2023-11-21 14:50:19 +0000

    Add subscriber callback types to exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 export { AutomergeStore } from "./automerge-store";
 export { AutomergeRepoStore } from "./automerge-repo-store";
 
-export type { AutomergeStoreOptions, UndoRedo } from "./automerge-store";
+export type {
+  AutomergeStoreOptions,
+  UndoRedo,
+  PatchData,
+  SubscribeCallback,
+} from "./automerge-store";
